1d7e00ed8d5b1149b665a6eb49d30710fa4089e5
LuisMurSal <al19760547@ite.edu.mx>
2024-11-12 11:45:31 -0800

    cambios en el sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -19,12 +19,11 @@ const Sidebar = () => {
   return (
     <div className="sidebar-container">
       <aside className={`sidebar-desktop ${isCollapsed ? 'collapsed' : ''}`}>
-        <Logo />
-        <button className="toggle-sidebar-btn" onClick={toggleSidebar}>
-          <FaBars />
-        </button>
 
         <ul className="menu">
+          <div className='logo-line'>
+          <Logo />
+          </div>
           <li><FaHome className="icon" /> <span className={`text ${isCollapsed ? 'hidden' : ''}`}>Inicio</span></li>
           <li><FaAddressCard className="icon" /> <span className={`text ${isCollapsed ? 'hidden' : ''}`}>Pacientes</span></li>
           <li><FaUser className="icon" /> <span className={`text ${isCollapsed ? 'hidden' : ''}`}>Perfil</span></li>
@@ -32,6 +31,9 @@ const Sidebar = () => {
           <li><FaAddressBook className="icon" /> <span className={`text ${isCollapsed ? 'hidden' : ''}`}>Historial</span></li>
           <li><FaUserInjured className="icon" /> <span className={`text ${isCollapsed ? 'hidden' : ''}`}>Servicios</span></li>
           <li onClick={handleLogout} className="logout"><FaSignOutAlt className="icon" /> <span className={`text ${isCollapsed ? 'hidden' : ''}`}>Salir</span></li>
+          <div className="icon-circle" onClick={toggleSidebar}>
+            <FaBars />
+          </div>
         </ul>
       </aside>
     </div>
