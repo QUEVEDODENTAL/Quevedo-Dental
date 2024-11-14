99aaf81ac1ac14a9f31ac4a24ce7f580992cd5ec
Fabian-005 <fabianpon@gmail.com>
2024-11-13 17:57:51 -0800

    Componente register

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs'); // Biblioteca para hashear contraseñas
 const jwt = require('jsonwebtoken'); // Biblioteca para manejar JSON Web Tokens
 const cookieParser = require('cookie-parser'); // Middleware para manejar cookies
 const cors = require('cors'); // Middleware para permitir solicitudes desde distintos dominios
+const { doctor, empleado } = require('./prisma/client');
 require('dotenv').config(); // Cargar variables de entorno
 
 // Inicialización de la aplicación de Express
@@ -24,35 +25,141 @@ app.use(cors({
 // Inicialización del cliente Prisma para interactuar con la base de datos
 const prismaClient = new prisma.PrismaClient();
 
-// Ruta para el registro de usuario
-app.post('/register', async (req, res) => {
-    // Desestructuración de los datos enviados en el cuerpo de la solicitud
-    const { email, password, isAdministrator, isDoctor, isEmployee } = req.body;
+// Registro de doctor con manejo de contraseña y transacción
+app.post('/register/doctor', async (req, res) => {
+    const {
+        name, lastName, age, birthDate, gender, specialty, address,
+        cellphone, curp, licenseNumber, sex, medicalLicense, email,
+        hireDate, password
+    } = req.body;
 
-    console.log('Datos de registro:', req.body); // Log para verificar datos recibidos
+    // Validación de campos obligatorios
+    if (!email || !name || !lastName || !password) {
+        return res.status(400).json({ error: 'Campos obligatorios faltantes' });
+    }
 
     try {
-        // Hashear la contraseña antes de guardarla en la base de datos
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Creación del nuevo usuario en la base de datos
-        const user = await prismaClient.usuarios.create({
-            data: {
-                Email: email,
-                Password: hashedPassword,
-                IsAdministrator: isAdministrator || false,
-                IsDoctor: isDoctor || false,
-                IsEmployee: isEmployee || false,
-            },
+        // Verificar si el email ya existe en la base de datos
+        const existingDoctor = await prismaClient.doctor.findUnique({
+            where: { Email: email },
         });
 
-        // Respuesta exitosa con los datos del usuario creado
-        res.status(201).json({ message: 'Usuario creado exitosamente!', user });
+        if (existingDoctor) {
+            return res.status(400).json({ error: "El correo electrónico ya está en uso." });
+        }
+
+        // Encriptar la contraseña
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Datos para el registro del doctor
+        const doctorData = {
+            Name: name,
+            LastName: lastName,
+            Age: age ? parseInt(age) : null,
+            BirthDate: birthDate ? new Date(birthDate) : null,
+            Gender: gender || null,
+            Specialty: specialty,
+            Address: address,
+            Cellphone: cellphone,
+            CURP: curp,
+            LicenseNumber: licenseNumber,
+            Sex: sex,
+            MedicalLicense: medicalLicense,
+            Email: email,
+            HireDate: hireDate ? new Date(hireDate) : new Date(),
+            Password: hashedPassword
+        };
+
+        // Datos para el usuario
+        const usuarioData = {
+            Email: doctorData.Email,
+            Password: doctorData.Password,
+            IsDoctor: true,
+        };
+
+        // Iniciar la transacción
+        const result = await prismaClient.$transaction(async (prisma) => {
+            // Crear el doctor
+            const doctor = await prisma.doctor.create({ data: doctorData });
+
+            // Crear el usuario para el doctor
+            const usuario = await prisma.usuarios.create({
+                data: usuarioData,
+            });
+
+            // Retornar ambos objetos si todo fue bien
+            return { doctor, usuario };
+        });
+
+        res.status(201).json({ message: 'Doctor y usuario registrados exitosamente', result });
     } catch (error) {
-        console.error('Error en el registro:', error); // Log del error en registro
-        res.status(500).json({ message: 'Error creando usuario', error });
+        console.error('Error al registrar doctor y usuario:', error);
+        res.status(500).json({ error: 'Error al registrar el doctor y el usuario' });
     }
 });
+
+// Registro de empleado con manejo de contraseña y transacción
+app.post('/register/employee', async (req, res) => {
+    const {
+        name, lastName, birthDate, gender, cellphone, email,
+        address, position, curp, rfc, salary, hireDate, password
+    } = req.body;
+
+    // Validación de campos obligatorios
+    if (!email || !name || !lastName || !password) {
+        return res.status(400).json({ error: 'Campos obligatorios faltantes' });
+    }
+
+    try {
+        // Encriptar la contraseña
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Datos para el registro del empleado
+        const employeeData = {
+            Name: name,
+            LastName: lastName,
+            BirthDate: birthDate ? new Date(birthDate) : null,
+            Gender: gender,
+            Cellphone: cellphone,
+            Email: email,
+            Address: address,
+            Position: position,
+            CURP: curp,
+            RFC: rfc,
+            Salary: salary ? parseFloat(salary) : null,
+            HireDate: hireDate ? new Date(hireDate) : new Date(),
+            Password: hashedPassword
+        };
+
+        // Datos para el usuario
+        const usuarioData = {
+            Email: employeeData.Email,
+            Password: employeeData.Password,
+            IsEmployee: true,
+        };
+
+        // Iniciar la transacción
+        const result = await prismaClient.$transaction(async (prisma) => {
+            // Crear el empleado
+            const employee = await prisma.empleado.create({ data: employeeData });
+
+            // Crear el usuario para el empleado
+            const usuario = await prisma.usuarios.create({
+                data: usuarioData,
+            });
+
+            // Retornar ambos objetos si todo fue bien
+            return { employee, usuario };
+        });
+
+        res.status(201).json({ message: 'Empleado y usuario registrados exitosamente', result });
+    } catch (error) {
+        console.error('Error al registrar empleado y usuario:', error);
+        res.status(500).json({ error: 'Error al registrar el empleado y el usuario' });
+    }
+});
+
+
 
 // Ruta de inicio de sesión
 app.post('/login', async (req, res) => {
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,290 @@
+import React, { useState } from 'react';
+import '../styles/Register.css';
+
+const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        lastName: '',
+        age: '',
+        birthDate: '',
+        gender: '',
+        specialty: '',
+        address: '',
+        cellphone: '',
+        curp: '',
+        licenseNumber: '',
+        sex: '',
+        medicalLicense: '',
+        email: '',
+        hireDate: '',
+        position: '',
+        rfc: '',
+        salary: '',
+        password: ''
+    });
+    const [userType, setUserType] = useState("doctor");
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    // Función para manejar el submit del formulario
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const url = userType === 'doctor' ? 'http://localhost:3000/register/doctor' : 'http://localhost:3000/register/employee';
+
+        const formDataWithUserType = { ...formData, userType };
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formDataWithUserType),
+            });
+
+            const data = await response.json();
+            if (response.ok) {
+                alert(`${userType.charAt(0).toUpperCase() + userType.slice(1)} registrado con éxito`);
+
+                setFormData({
+                    name: '',
+                    lastName: '',
+                    age: '',
+                    birthDate: '',
+                    gender: '',
+                    specialty: '',
+                    address: '',
+                    cellphone: '',
+                    curp: '',
+                    licenseNumber: '',
+                    sex: '',
+                    medicalLicense: '',
+                    email: '',
+                    hireDate: '',
+                    password: ''
+                });
+
+            } else {
+                alert('Error: ' + data.error);
+            }
+        } catch (error) {
+            console.error('Error al registrar usuario:', error);
+            alert('Hubo un problema al registrar el usuario');
+        }
+    };
+
+
+    return (
+        <div className="register-container">
+            <h2>Registro de {userType === "doctor" ? "Doctor" : "Empleado"}</h2>
+            <div className="form-container">
+                <div className="user-type-selector">
+                    <label>
+                        <input
+                            type="radio"
+                            value="doctor"
+                            checked={userType === "doctor"}
+                            onChange={() => setUserType("doctor")}
+                        />
+                        Doctor
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="employee"
+                            checked={userType === "employee"}
+                            onChange={() => setUserType("employee")}
+                        />
+                        Empleado
+                    </label>
+                </div>
+                <form onSubmit={handleSubmit} className="horizontal-form">
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Nombre"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="lastName"
+                            placeholder="Apellido"
+                            value={formData.lastName}
+                            onChange={handleChange}
+                        />
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                        <input
+                            type="date"
+                            name="birthDate"
+                            placeholder="Fecha de nacimiento"
+                            value={formData.birthDate}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    {userType === "doctor" && (
+                        <div className="form-group">
+                            <input
+                                type="number"
+                                name="age"
+                                placeholder="Edad"
+                                value={formData.age}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="text"
+                                name="specialty"
+                                placeholder="Especialidad"
+                                value={formData.specialty}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="text"
+                                name="address"
+                                placeholder="Dirección"
+                                value={formData.address}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="text"
+                                name="cellphone"
+                                placeholder="Teléfono"
+                                value={formData.cellphone}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="text"
+                                name="curp"
+                                placeholder="CURP"
+                                value={formData.curp}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="text"
+                                name="licenseNumber"
+                                placeholder="Número de licencia"
+                                value={formData.licenseNumber}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="text"
+                                name="sex"
+                                placeholder="Sexo"
+                                value={formData.sex}
+                                onChange={handleChange}
+                            />
+                            <label>Género</label>
+                            <select
+                                name="gender"
+                                value={formData.gender}
+                                onChange={handleChange}
+                            >
+                                <option value="">Seleccione género</option>
+                                <option value="Masculino">Masculino</option>
+                                <option value="Femenino">Femenino</option>
+                                <option value="Otro">Otro</option>
+                            </select>
+                            <input
+                                type="date"
+                                name="hireDate"
+                                placeholder="Fecha de contratación"
+                                value={formData.hireDate}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Ingresa contraseña"
+                                value={formData.password}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    )}
+                    {userType === "employee" && (
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                name="position"
+                                placeholder="Posición"
+                                value={formData.position}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="text"
+                                name="rfc"
+                                placeholder="RFC"
+                                value={formData.rfc}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="number"
+                                name="salary"
+                                placeholder="Salario"
+                                value={formData.salary}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="text"
+                                name="address"
+                                placeholder="Dirección"
+                                value={formData.address}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="text"
+                                name="cellphone"
+                                placeholder="Teléfono"
+                                value={formData.cellphone}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="text"
+                                name="curp"
+                                placeholder="CURP"
+                                value={formData.curp}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="date"
+                                name="hireDate"
+                                placeholder="Fecha de contratación"
+                                value={formData.hireDate}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Ingresa contraseña"
+                                value={formData.password}
+                                onChange={handleChange}
+                            />
+                            <label>Género</label>
+                            <select
+                                name="gender"
+                                value={formData.gender}
+                                onChange={handleChange}
+                            >
+                                <option value="">Seleccione género</option>
+                                <option value="Masculino">Masculino</option>
+                                <option value="Femenino">Femenino</option>
+                                <option value="Otro">Otro</option>
+                            </select>
+                        </div>
+                    )}
+                    <button type="submit" className="register-button">Registrar</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
